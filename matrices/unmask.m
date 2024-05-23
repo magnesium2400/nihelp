afb4e2fb6d9963340341495ea5c4e77c33365466
magnesium2400 <mehulrandom@gmail.com>
2024-05-23 14:15:01 +1000

    add unmask

---
+++ matrices/unmask.m
@@ -0,0 +1,23 @@
+function out = unmask(mask, data, fillVal)
+%% UNMASK Adds empty rows to data (which may have been removed previously)
+%% Examples
+%   unmask([1 0 1 0 1 0], (1:3)')
+%   unmask([1 0 1 0 1 0 1 0]', magic(4), 0)
+% 
+% 
+%% TODO
+% * docs
+% 
+% 
+%% Authors
+% Mehul Gajwani, Monash University, 2024
+% 
+% 
+
+assert((numel(mask)==length(mask)) && (nnz(mask) == size(data, 1))); 
+if nargin < 3 || isempty(fillVal); fillVal = nan; end
+
+out = ones(numel(mask), size(data, 2)) * fillVal; 
+out(logical(mask(:)),:) = data; 
+
+end
