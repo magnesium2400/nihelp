412fe08912fb9247d3f2a38fdf9a8f4441eed278
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2024-05-27 17:08:16 +1000

    Create addColorbarLine.m

---
+++ plotting/addColorbarLine.m
@@ -0,0 +1,30 @@
+function ax = addColorbarLine(cb, yval, linestyle)
+%% ADDCOLORBARLINE Adds horizontal line to colorbar at specified value
+%% Examples
+%   figure; imagesc(magic(3)); cb = colorbar; addColorbarLine(cb,3); 
+%   figure; imagesc(magic(3)); cb = colorbar; addColorbarLine(cb,3, {'-.', 'LineWidth', 2}); 
+% 
+% 
+%% Usage Notes
+% This is quite a simple function, but its functionality is somewhat unstable.
+% In particular, the line will not be correctly placed if the width of the
+% figure is changed after generation of the line.
+%
+%
+%% TODO
+% * docs
+% 
+% 
+%% Authors
+% Mehul Gajwani, Monash University, 2024
+% 
+% 
+
+if nargin < 3 || isempty(linestyle); linestyle = {}; end
+
+ax = axes('Position', cb.Position, 'Units', cb.Units);
+axis(ax, 'off'); 
+ylim(ax, cb.Limits); 
+yline(ax, yval, linestyle{:}); 
+
+end
