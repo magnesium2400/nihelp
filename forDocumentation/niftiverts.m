a216d34e5429fe01a29ba5ba9d3d8fc568b4a225
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2025-07-22 12:44:24 +1000

    replace nitftiverts with nifti2verts

--- forDocumentation/niftiverts.m
+++
@@ -1,7 +0,0 @@
-function verts = niftiverts(filename)
-
-V = niftiread(filename); 
-nt = niftiinfo(filename).Transform.T; 
-verts = affineVerts(vol2xyz(V,logical(V))-1, nt, 1); 
-
-end
--- forDocumentation/nrrdverts.m
+++
@@ -1,7 +0,0 @@
-function verts = nrrdverts(filename)
-
-V = nrrdread(filename); 
-nt = nrrdinfo(filename).SpatialMapping.A; 
-verts = affineVerts(vol2xyz(V,logical(V)), nt, 1); 
-
-end
