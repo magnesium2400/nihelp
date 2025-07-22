9502f0e091691d9f2aa10cca85675351ee5eeecc
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2025-07-22 12:07:23 +1000

    add mask option to volume processing

--- inDevelopment/V2v.m
+++ inDevelopment/V2v.m
@@ -1,5 +1,9 @@
-function out = V2v(V, tform, isNiftiTform)
-if nargin < 2 || isempty(tform);        tform        = eye(4); end
-if nargin < 3 || isempty(isNiftiTform); isNiftiTform = false;  end
-out = affineVerts(vol2xyz(V,logical(V))-1, tform, isNiftiTform); 
+function [v,d] = V2v(V, tform, isNiftiTform, mask)
+%% V2v Volume to vertices, assumes that Volume should be 0-indexed
+if nargin < 2 || isempty(tform);        tform        = eye(4);  end
+if nargin < 3 || isempty(isNiftiTform); isNiftiTform = false;   end
+if nargin < 4;                          mask         = [];      end
+mask = processMask(V,mask); 
+v = affineVerts(vol2xyz(V,mask)-1, tform, isNiftiTform); 
+d = V(mask); 
 end
