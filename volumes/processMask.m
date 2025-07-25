4c447e368bd460ca2626b989626dd667065e967c
chrisadamsonmcri <chris.adamson@mcri.edu.au>
2025-07-25 15:36:54 +1000

    Documentation and minor fixes

--- volumes/processMask.m
+++ volumes/processMask.m
@@ -1,10 +1,17 @@
 function out = processMask(V, mask)
-%% Syntax
-%  out = processMask(V); % Volume must be numeric or logical
-%  out = processMask(mask); % mask mask be numeric or logical
-%  out = processMask(V, mask); % Volume must be numeric/logical, mask mask be function handle/numeric/logical
+% Converts a numeric or logical volume into a logical array,
+% or apply a mask to the volume.
 %
+% Inputs:
+%   V     - A numeric or logical array representing the volume.
+%   mask  - (Optional) A numeric or logical array or a function handle 
+%           that defines the mask to be applied to the volume.
 %
+% Outputs:
+%   out   - A logical array resulting from the processing of the volume 
+%           and mask.
+%
+
 
 assert(isnumeric(V) || islogical(V));
 
@@ -13,10 +20,16 @@ if nargin==1
 end
 
 if nargin==2
-    if      isa(mask, 'function_handle');   out = mask(V);
-    elseif  isnumeric(mask);                out = logical(mask);
-    elseif  islogical(mask);                out = mask;
-    else;   error('Supplied mask must be logical matrix, numeric matrix, or function handle+volume');
+    if isempty(mask)
+        out = logical(V);
+    elseif isa(mask, 'function_handle')
+        out = mask(V);
+    elseif isnumeric(mask)
+        out = logical(mask);
+    elseif islogical(mask)
+        out = mask;
+    else
+        error('Supplied mask must be logical matrix, numeric matrix, or function handle+volume');
     end
 end
 
